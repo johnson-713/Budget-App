9eebfe91baee9812eceb97db2d790c7099f8acc2
johnson-713 <johnsonelangovan@gmail.com>
2023-09-25 22:12:54 +0530

    Budget form

--- budget/src/Pages/Dashboard.js
+++ budget/src/Pages/Dashboard.js
@@ -6,11 +6,13 @@ import { useLoaderData } from 'react-router-dom';
 import { fetchData } from '../helpers';
 import Intro from '../components/Intro';
 import { toast } from 'react-toastify';
+import AddBudgetForm from '../components/AddBudgetForm';
 
 //loader
 export function dashboardLoader(){
     const userName = fetchData("userName");
-    return { userName }
+    const budgets = fetchData("budgets");
+    return { userName, budgets }
 }
 
 // action 
@@ -26,11 +28,25 @@ export async function dashboardAction({request}){
 }
 
 const Dashboard = () => {
-  const { userName } = useLoaderData();
+  const { userName, budgets } = useLoaderData();
   return (
-    <div>
-      { userName ? (<p>{ userName }</p>) : <Intro /> }
-    </div>
+    <>
+      { userName ? (
+      <div className='dashboard'>
+        <h1>Welcome back, 
+          <span className='accent'>{ userName }</span>
+        </h1>
+        <div className='grid-sm'>
+          {/* {budgets ? () : ()} */}
+          <div className='grid-lg'>
+            <div className='flex-lg'>
+              <AddBudgetForm />
+            </div>
+          </div>
+
+        </div>
+      </div>) : <Intro /> }
+    </>
   )
 }
 
--- budget/src/Pages/Error.js
+++ budget/src/Pages/Error.js
@@ -1,9 +1,31 @@
+import { ArrowUturnLeftIcon, HomeIcon } from '@heroicons/react/24/solid';
 import React from 'react'
+import { Link, useNavigate, useRouteError } from 'react-router-dom';
 
 const Error = () => {
+  const error = useRouteError();
+  const navigate = useNavigate();
+
   return (
-    <div>
-      Error
+    <div className='error'>
+      <h1>Uh oh! We've got a problem</h1>
+      <p>{error.message || error.statusText}</p>
+      <div className='flex-md'>
+        <button 
+          className='btn btn--dark'
+          onClick={() => navigate(-1)} // It goes back one page after clicking the button
+        >
+          <ArrowUturnLeftIcon width={20} /> 
+          <span>Go back</span>
+        </button>
+        <Link
+          to="/"
+          className='btn btn--dark'
+        >
+        <HomeIcon width={20} />
+        <span>Go home</span>
+        </Link>
+      </div>
     </div>
   )
 }
